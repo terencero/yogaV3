11d7392b1db24145c15d2c83f9c916d932260687
Terence Ro <rowyourboat83@gmail.com>
2018-04-28 17:33:47 -0500

    update basic vue router structure

---
+++ src/config/routes.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import About from '../components/About';
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+  routes = [{
+    path: '/about',
+    component: About,
+  }]
+})
