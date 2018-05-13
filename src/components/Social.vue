b31da056fe5822f4f04a4a49a82923fae03cc90b
Terence Ro <rowyourboat83@gmail.com>
2018-05-13 11:03:47 -0500

    port over to axios for both client and server side http requests

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    empty
     <img src="" alt="" />
     <img src="" alt="" />
     <img src="" alt="" />
@@ -10,9 +11,22 @@
 </template>
 
 <script>
-import axios from 'axios';
-export default {
-  
+import axios from "axios";
+function getInstagramPosts() {
+  axios
+    .get("/yogaInstagram")
+    .then(resp => {
+      console.log("instagram res", resp);
+    })
+    .catch(err => console.log('log error',err));
 }
+export default {
+  beforeMount() {
+    this.getInstagramPosts();
+  },
+  methods: {
+    getInstagramPosts
+  }
+};
 </script>
 
